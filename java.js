68920f46d564704b8ba17d946a2e9491f7d78eac
Brian Morse <brian.p.morse@gmail.com>
2023-02-21 22:35:07 -0800

    Some fixes but app still does not work properly

--- java.js
+++ java.js
@@ -1,19 +1,33 @@
 // set variable to store time remaining
-var timeLeft = 59;
+var timeLeft = 60;
+var currentAnswer = "";
+
+ //Adds score to local storage
+ var playerName = prompt("What are your initials?");
+
+//variables to use to adjust score and check user answers
+var score = parseInt(document.getElementById("correctAnswers").value);
+var check = document.getElementById("userAnswer");
+
+ function highScore(){
+ localStorage.setItem(playerName, score)
+ };
 
 //create varible to store function for determining timeLeft
 function countDown() { setInterval(function(){
     //gives instruction for when timeLeft passes 0 and clears the interval from setInterval
     if(timeLeft < 0){
-        clearInterval(countDown);
         document.getElementById("timeLeft").innerHTML = "Time is up!!";
+        clearInterval(countDown);
+        highScore();
+        
     } else {
-        document.getElementById("timeLeft").innerHTML = timeLeft;
+        timeLeft--;
+        document.getElementById("timeLeft").innerHTML = timeLeft ;
     }
-    timeLeft -= 1;
-
+    
 }, 1000);
-}
+};
 document.getElementById("start").addEventListener("click", countDown);
 document.getElementById("start").addEventListener("click", currentQuestion);
 
@@ -27,31 +41,48 @@ document.getElementById("start").addEventListener("click", currentQuestion);
         ["True or False, the javascript file can be linked in the head or the body of html?", "True"],
         ["True or False, javascript can automatically use Jquerry functions?", "False"],
     ];
+
+
+    
 //create function to select random question
 function currentQuestion(){
-    var nextQuestions = questions[Math.floor(Math.random()*questions.length)][0];
-       
+    var randomQuestion = questions[Math.floor(Math.random() * questions.length)]
+
+    var nextQuestions = randomQuestion[0]
+    var nextAnswer = randomQuestion[1]
+    
+document.getElementById("currentAnswer").innerHTML = nextAnswer;   
+currentAnswer = nextAnswer;
 document.getElementById("question").innerHTML = nextQuestions;
-};
+}
+
+//create function to adjust score
+  function adjustScore(){
+        document.getElementById("correctAnswers").innerHTML = num.toString(score + 1);
+    }
+
+   
 
 //create function to check for correct answer
 
-function checkAnswer(){
-    var score = document.getElementById("correctAnswers");
-    
+
+function checkAnswer(event){
+    event.preventDefault();
+    console.log(check.value);
+  document.getElementById("currentAnswer").value
     if(
-    document.getElementById("userAnswer") = questions[1]){
-        document.getElementById("correctAnswers").innerHTML =  score += 
+   check.value == currentAnswer){
+        adjustScore();
+        
 }
     else {
-        score = score-=
+       timeLeft = timeLeft - 5 ;
 
         }
-
+        
     }
 
-}
-;
+
 
 //add event listener and code for submit button
 document.getElementById("submit").addEventListener("click", checkAnswer);
